91e9fb3f7e3e07c41fb409715e3fb7392fcd6dab
David-DMG <david.dmg@mail.com>
2017-06-10 22:20:08 +0100

    improved variable names

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -7,61 +7,59 @@
 int main(int argc, string argv[])
 {
     if (argc != 2)
-        {
-        // return 1 and error msg for no key
-        printf("You forgot to enter the encryption key!\n");
+    {
+        // return 1 and error msg
+        printf("Error: must enter a command line cipher key!\n");
         return 1;
-        }
+    }
         
     //declaring variables
-    int key = atoi(argv[1]);
+    int cipher_key = atoi(argv[1]);
     string plaintxt;
-    int cyphered;
-    int letter;
+    int ciphered_char;
+    int plaintxt_char;
     
-    if (key < 1)
+    if (cipher_key < 1)
     {
-        // return 1 and error msg for -ve key
-        printf("Your encryption key must be a non-negative interger!\n");
+        // return 1 and error msg
+        printf("Error: Your cipher key must be a non-negative interger!\n");
         return 1;
     }
     
-    else if (argc == 2)
-    {
-        // promt for txt to encrypt
-        printf("plaintext: ");
-        plaintxt = get_string();
-        printf("ciphertext: ");
+    // promt for txt to encrypt
+    printf("plaintext: ");
+    plaintxt = get_string();
+    printf("ciphertext: ");
         
-        // loop for cycling though characters of string 'plaintxt'
-        for (int i = 0, n = strlen(plaintxt); i < n; i++)
+    // loop for cycling though characters of string 'plaintxt'
+    for (int i = 0, n = strlen(plaintxt); i < n; i++)
+    {
+
+        plaintxt_char = plaintxt[i];
+         
+        //if its a plaintxt_char
+        if (isalpha(plaintxt_char))
         {
-            //assigning 'i'th character variable
-            letter = plaintxt[i];
-            
-            //if its a letter
-            if (isalpha(letter))
+            //if its lower case encrypt 'plaintxt_char' using cipher_key, use %26 to roll over numbers is 26 range, and then apply to lower case ascii range
+            if (islower(plaintxt_char))
             {
-                //if its lower case encrypt 'letter' using key, use %26 to roll over numbers is 26 range, and then apply to lower case ascii range
-                if (islower(letter))
-                {
-                cyphered = (((letter - 97 + key) % 26) + 97);
-                printf("%c", cyphered);
-                }
-                //if its upper case encrypt 'letter' using key, use %26 to roll over numbers is 26 range, and then apply to upper case ascii range
-                else if (isupper(letter))
-                {
-                cyphered = (((letter - 65 + key) % 26) + 65);
-                printf("%c", cyphered);
-                }
+            ciphered_char = (((plaintxt_char - 97 + cipher_key) % 26) + 97);
+            printf("%c", ciphered_char);
             }
-            //if not alhpa print the letter unencrypted
-            else
+            //if its upper case encrypt 'plaintxt_char' using cipher_key, use %26 to roll over numbers is 26 range, and then apply to upper case ascii range
+            else if (isupper(plaintxt_char))
             {
-                printf("%c", letter);
+            ciphered_char = (((plaintxt_char - 65 + cipher_key) % 26) + 65);
+            printf("%c", ciphered_char);
             }
         }
-        printf("\n");
-        return 0;
+        //if not alhpa print the plaintxt_char unencrypted
+        else
+        {
+            printf("%c", plaintxt_char);
+            }
     }
+    printf("\n");
+    return 0;
 }
+
